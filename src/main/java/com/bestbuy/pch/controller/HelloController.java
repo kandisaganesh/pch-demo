e7dea0369e407120873ac4ba325f514d4ed8ab66
kandisaganesh <kandisa.ganesh@gmail.com>
2012-10-24 16:41:21 +0000

    Removing one attribute

--- src/main/java/com/bestbuy/pch/controller/HelloController.java
+++ src/main/java/com/bestbuy/pch/controller/HelloController.java
@@ -11,12 +11,7 @@ public class HelloController {
 
   @RequestMapping(method = RequestMethod.GET)
   public String printWelcome(ModelMap model) {
-
-<<<<<<< .mine
     model.addAttribute("message", "Spring 3 MVC Hello World changed in branch1");
-=======
-    model.addAttribute("message", "Spring 3 MVC Hello World Changed in branch2");
->>>>>>> .theirs
     return "hello";
   }
 }
