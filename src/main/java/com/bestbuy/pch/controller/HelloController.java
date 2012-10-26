212fdaac68864c3ccef76e7714285c0efd068c70
kandisaganesh <kandisa.ganesh@gmail.com>
2012-10-26 15:34:27 -0500

    Removing PMD vioaltions

--- src/main/java/com/bestbuy/pch/controller/HelloController.java
+++ src/main/java/com/bestbuy/pch/controller/HelloController.java
@@ -14,9 +14,7 @@ int count = 0;
   public String printWelcome(ModelMap model) {
 	System.out.println(++count);
     model.addAttribute("message", "Spring 3 MVC Hello World changed in branch1");
-	System.out.println("Adding PMD"):
+	System.out.println("Adding PMD");
 	return "hello";
   }
-
-
 }
